33808236d9a4972f5b837ec78270a859e0d8b6c9
TEJA SANJEEV <chtsanjeev2003@gmail.com>
2023-12-28 16:58:13 +0530

    written middlewareCode

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from exppress;
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from cors;
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import Path  from "path";
+import { fileURLToPath } from "url";
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+dotenv.config();
+
+const app=express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+const storage=multer.diskStorage({
+     destination: function (req,file,cb){
+        cb(null,"public/assets");
+     },
+     filename: function(req,file,cb){
+        cb(null,file.originalname);
+     }
+})
+
+const upload = multer({storage} )
+
+// Mongoose setup
+
+const PORT = process.env.PORT || 6001;  // we are accessing portNumber from .env file,which we have stored
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParse: true,
+    useunifiedTopology:true,
+})
+.then(()=>{
+    app.listen(PORT,()=>console.log(`server port :${PORT}`));
+})
